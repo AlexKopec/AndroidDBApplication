45c0f168bd49e922452df4f695e11da958989a51
Alex Kopec <kopecalex@gmail.com>
2018-10-31 19:44:32 -0500

    added onCreate method to DB Handler

--- app/src/main/java/m/kopec/databaseapplication/MyDBHandler.java
+++ app/src/main/java/m/kopec/databaseapplication/MyDBHandler.java
@@ -1,5 +1,7 @@
 package m.kopec.databaseapplication;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 /**
@@ -10,5 +12,38 @@ public class MyDBHandler extends SQLiteOpenHelper {
     // information of database
     private static final int DATABASE_VERSION = 1;
     private static final String DATABASE_NAME = "studentDB.db";
-    public static final String TABLE_NAME = "student"
+    public static final String TABLE_NAME = "Student";
+    public static final String COLUMN_ID = "StudentID";
+    public static final String COLUMN_NAME = "StudentName";
+
+    // initialize the database
+    public MyDBHandler(Context context, Stringname, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, DATABASE_NAME, factory, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_TABLE = "CREATE TABLE" + TABLE_NAME + "(" + COLUMN_ID + ")" + "INTEGER PRIMARYKEY," + COLUMN_NAME + "TEXT )";
+        db.execSQL(CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+    }
+
+    public String loadHandler() {
+    }
+
+    public void addHandler(Student student) {
+    }
+
+    public Student findHandler(String studentname) {
+    }
+
+    public boolean deleteHander(int ID) {
+    }
+
+    public boolean updateHandler(int ID, String name) {
+    }
+
 }
