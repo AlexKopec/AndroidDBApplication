42974d26099b64d31bf8f246bf73179f13298b2b
Alex Kopec <kopecalex@gmail.com>
2018-10-31 19:07:50 -0500

    init repo

---
+++ app/src/main/java/m/kopec/databaseapplication/MyDBHandler.java
@@ -0,0 +1,14 @@
+package m.kopec.databaseapplication;
+
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by kopec on 10/31/2018.
+ */
+
+public class MyDBHandler extends SQLiteOpenHelper {
+    // information of database
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "studentDB.db";
+    public static final String TABLE_NAME = "student"
+}
---
+++ app/src/main/java/m/kopec/databaseapplication/Student.java
@@ -0,0 +1,36 @@
+package m.kopec.databaseapplication;
+
+/**
+ * Created by kopec on 10/31/2018.
+ */
+
+public class Student {
+    // fields
+    private int studentID;
+    private String studentName;
+
+    // constructors
+    public Student() {
+    }
+
+    public Student(int id, String studentname) {
+        this.studentID = id;
+        this.studentName = studentname;
+    }
+
+    // properties
+    public void setID(int id) {
+        this.studentID = id;
+    }
+    public int getID() {
+        return this.studentID;
+    }
+
+    public void setStudentName(String studentname) {
+        this.studentName = studentname;
+    }
+
+    public String getStudentName() {
+        return this.studentName;
+    }
+}
